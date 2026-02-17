35eb60a477b5a8c0977250b2f188febcc6a094ea
Ishant Bhoyar <ishantbhoyar59@gmail.com>
2026-02-18 05:04:39 +0530

    update graph

--- Lab1_202410101200059/Graph.java
+++ Lab1_202410101200059/Graph.java
@@ -1,29 +1,40 @@
-import java.util.ArrayList;
+import java.util.*;
 public class Graph {
     private int vertices_count;
-    private ArrayList<ArrayList<Integer>> adjacency_list;
+    private ArrayList<ArrayList<Integer>> connections;
 
 
 
-Graph(int vertices_count){
-    if(vertices_count < 0)
-        throw new IllegalArgumentException("Vertices count cannot be negative");
-    this.vertices_count=vertices_count;
-    for(int i=0; i<vertices_count; i++){
-        adjacency_list.add(new ArrayList<>());
+    Graph(int vertices){
+        
+        if(vertices < 0)
+            throw new IllegalArgumentException("Vertices count cannot be negative");
+        
+        this.vertices_count = vertices;
+
+        connections = new ArrayList<>();
+
+        for(int i=0; i<vertices_count; i++){
+            connections.add(new ArrayList<>());
+        }
     }
-}
 
-public int getVerticesCount(){
-    return vertices_count;
-}
+    public int getVerticesCount(){
+        return vertices_count;
+    }
 
-public void addEdge(int source, int destination){
-    if(source < 0 || source >= vertices_count || destination < 0 || destination >= vertices_count)
-        throw new IllegalArgumentException("Source and destination must be between 0 and " + (vertices_count - 1));
-    adjacency_list.get(source).add(destination);
-    adjacency_list.get(destination).add(source);
+    public void addEdge(int source, int destination){
+        if(source < 0 || source >= vertices_count || destination < 0 || destination >= vertices_count)
+            throw new IllegalArgumentException("Source and destination must be between 0 and " + (vertices_count - 1));
+        
+        if(!connections.get(source).contains(destination)){
+            connections.get(source).add(destination);
+        }
 
-}
+        if(!connections.get(destination).contains(source)){
+            connections.get(destination).add(source);
+        }
+
+    }
 
 }
